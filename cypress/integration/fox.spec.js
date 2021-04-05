e590993c3055ae4e73e57fc33981aaf6022559ee
Arka Sain <arka.sain@aol.com>
2021-04-05 20:28:09 +0200

    Implements the beforeEach hook in Cypress

---
+++ cypress/integration/fox.spec.js
@@ -0,0 +1,41 @@
+describe("Aliasing using Cypress - Extended", () => {
+
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/example-3");
+
+    cy.get('[data-cy="last-name-chars-left-count"]').as("charsLeftSpan");
+    cy.get('[data-cy="input-last-name"]').as("charsLeftInput");
+  });
+
+
+  it("displays the appropriate remaining characters count", () => {
+    /* Another way to work with Aliasing ... */
+    cy.get("@charsLeftSpan").then(($charsLeftSpan) => {
+      /* `$charsLeftSpan` -> Denotes the DOM Element that Cypress returns to us ... */
+      expect($charsLeftSpan.text()).to.equal("15");
+    });
+
+    /* Checks the initial state ... */
+    cy.get("@charsLeftSpan").invoke("text").should("equal", "15");
+
+    /* Type in a small word ... */
+    cy.get("@charsLeftInput").type("hello");
+
+    cy.get("@charsLeftSpan").invoke("text").should("equal", "10");
+
+    /* Type in to the maximum limit ... */
+    cy.get("@charsLeftInput").type(" my friend");
+
+    cy.get("@charsLeftSpan").invoke("text").should("equal", "0");
+  });
+
+  it("prevents the user from typing more characters once max limit is reached", () => {
+    cy.get("@charsLeftInput").type("abcdefghijklmnopqrstuvwxyz");
+
+    /* Checking the input element too ... */
+    cy.get("@charsLeftInput").should("have.attr", "value", "abcdefghijklmno");
+
+    /* Checking the remaining characters ... */
+    cy.get("@charsLeftSpan").invoke("text").should("equal", "0");
+  });
+});
