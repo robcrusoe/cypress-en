fb4851f098aaf07b96871716f54a4fdea7867b17
Arka Sain <arka.sain@aol.com>
2021-04-05 21:23:34 +0200

    Adds a debugger in Cypress

--- cypress/integration/giraffe.spec.js
+++ cypress/integration/giraffe.spec.js
@@ -34,7 +34,10 @@ describe('Basic Page Interactions', () => {
 
     /* working with trigger-commands */
     it('works with the different trigger commands provided by cypress', () => {
-        cy.get('[data-cy="box-4-items-list"] > :nth-child(2)').trigger('mouseover', 10, 20);
+        cy.get('[data-cy="box-4-items-list"] > :nth-child(2)').trigger('mouseover', 10, 20)
+            .then(() => {
+                debugger;
+            });
 
         cy.get('[data-cy="box-4-selected-name"]')
             .invoke('text')
