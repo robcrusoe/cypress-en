919a52adebe2f5404a375a31c1220a72e3017cc7
Arka Sain <arka.sain@aol.com>
2021-04-05 20:18:51 +0200

    Makes use of Aliasing with Cypress

---
+++ cypress/integration/deer.spec.js
@@ -0,0 +1,48 @@
+describe("Aliasing using Cypress", () => {
+  it("displays the appropriate remaining characters count", () => {
+    cy.visit("http://localhost:3000/example-3");
+
+    cy.get('[data-cy="last-name-chars-left-count"]').as('charsLeftSpan');
+    cy.get('[data-cy="input-last-name"]').as('charsLeftInput');
+
+    /* Checks the initial state ... */
+    cy.get('@charsLeftSpan')
+      .invoke("text")
+      .should("equal", "15");
+
+    /* Type in a small word ... */
+    cy.get('@charsLeftInput').type("hello");
+
+    cy.get('@charsLeftSpan')
+      .invoke("text")
+      .should("equal", "10");
+
+    /* Type in to the maximum limit ... */
+    cy.get('@charsLeftInput').type(" my friend");
+
+    cy.get('@charsLeftSpan')
+      .invoke("text")
+      .should("equal", "0");
+  });
+
+  it("prevents the user from typing more characters once max limit is reached", () => {
+    cy.visit("http://localhost:3000/example-3");
+
+    cy.get('[data-cy="last-name-chars-left-count"]').as('charsLeftSpan');
+    cy.get('[data-cy="input-last-name"]').as('charsLeftInput');
+
+    cy.get('@charsLeftInput').type("abcdefghijklmnopqrstuvwxyz");
+
+    /* Checking the input element too ... */
+    cy.get('@charsLeftInput').should(
+      "have.attr",
+      "value",
+      "abcdefghijklmno"
+    );
+
+    /* Checking the remaining characters ... */
+    cy.get('@charsLeftSpan')
+      .invoke("text")
+      .should("equal", "0");
+  });
+});
